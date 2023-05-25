93397ba417f769e821d1cecc5abf68430aea7b37
OLLY27 <127981767+OLLY27@users.noreply.github.com>
2023-05-25 17:31:39 +0100

    Update env.sh

--- config/env.sh
+++ config/env.sh
@@ -8,7 +8,7 @@ export CSV_DIR=${ADV_KDB_HOME}/csv
 #Tickerplant Settings
 export TP_PORT=7001
 export TP_LOG_LOCATION=${ADV_KDB_HOME}/logs/tplog
-export Q_SCHEMA=${ADV_KDB_HOME}/tick/schema.q
+export Q_SCHEMA=schema
 export TP_TIMER=1
 
 #Realtime Database Settings
